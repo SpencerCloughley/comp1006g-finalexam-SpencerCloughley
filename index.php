9cdb0a815966e5dc5d2e870962d3247f8e4d1e51
SpencerCloughley <spencercloughley@gmail.com>
2023-04-20 14:27:16 -0400

    Fixed github link

--- index.php
+++ index.php
@@ -13,8 +13,11 @@ require 'include/header.php';
         <a href="https://wikipedia.org" target="_blank">Wikipedia</a>.
     </p>
     <p>Created my logo at <a href="LogoMakr.com" target="_blank">LogoMakr.com</a>.</p>
-    <p>https://github.com/SpencerCloughley/comp1006g-finalexam-SpencerCloughley</p>
-    
+    <p>
+    <a href="https://github.com/SpencerCloughley/comp1006g-finalexam-SpencerCloughley" target="_blank">
+    https://github.com/SpencerCloughley/comp1006g-finalexam-SpencerCloughley
+    </a>
+    </p>
     <div style="text-align:center;padding:1em 0;"> <h4><a style="text-decoration:none;" 
     href="https://www.zeitverschiebung.net/en/city/5894171"><span style="color:gray;">
     Current local time in</span><br />Barrie, Canada</a></h4> 
